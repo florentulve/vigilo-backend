2a963f7e1a079a150ef7c524b2aac188f199c99d
Florent Ulvé <florent.ulve@protonmail.com>
2022-09-26 20:58:30 +0200

    backup cellar

--- clevercloud/backup.sh
+++ clevercloud/backup.sh
@@ -6,5 +6,22 @@ export RCLONE_CONFIG_MYS3_ACCESS_KEY_ID=$CELLAR_ADDON_KEY_ID
 export RCLONE_CONFIG_MYS3_SECRET_ACCESS_KEY=$CELLAR_ADDON_KEY_SECRET
 export RCLONE_CONFIG_MYS3_ENDPOINT=$CELLAR_ADDON_HOST
 export RCLONE_CONFIG_MYS3_TYPE="s3"
+export RCLONE_CONFIG_MYS3_PROVIDER="AWS"
 
-./clevercloud/rclone sync ./data/images mys3:images.vigilo.lorient.dev --progress --s3-acl=public-read --header "Content-Encoding: gzip"
+DOW=$(date +%u)
+
+image_backup=./clevercloud/images-"${DOW}".tar.gz
+caches_backup=./clevercloud/caches-"${DOW}".tar.gz
+maps_backup=./clevercloud/maps-"${DOW}".tar.gz
+
+tar -zcvf "${image_backup}" ./data/images
+tar -zcvf "${caches_backup}" ./data/caches
+tar -zcvf "${maps_backup}" ./data/maps
+
+./clevercloud/rclone copy "${image_backup}" mys3:"${VG_BACKUP_BUCKET_NAME}" --progress --s3-acl=public-read --header "Content-Encoding: gzip"
+./clevercloud/rclone copy "${caches_backup}" mys3:"${VG_BACKUP_BUCKET_NAME}" --progress --s3-acl=public-read --header "Content-Encoding: gzip"
+./clevercloud/rclone copy "${maps_backup}" mys3:"${VG_BACKUP_BUCKET_NAME}" --progress --s3-acl=public-read --header "Content-Encoding: gzip"
+
+rm "${image_backup}"
+rm "${caches_backup}"
+rm "${maps_backup}"
